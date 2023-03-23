b9e1ab54d5d13037d379ff3fef1979eeef1b966f
Donnell Reuben <donnell.reuben@icloud.com>
2023-03-23 16:32:41 -0500

    first py file

---
+++ main.py
@@ -0,0 +1,14 @@
+print("""
+Donnell Reuben
+March 23rd 2023
+I am signing up for Replit's 100 days of Python 
+challenge!
+I will make sure to spend some time every day coding
+along, for a minimum of 10 minutes a day. 
+I'll be using Replit, an amazing online IDE so I can do 
+this from my phone wherever I happen to be. No excuses
+fro not coding from the middle of a field!
+""")
+
+print("I am feeling 🧐")
+print("You can follow my progress at replit.com/@donnellreuben")
