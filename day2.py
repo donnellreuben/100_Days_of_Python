d517dcb2a71bb3272345a28fb1eb33bcff9500fa
Donnell Reuben <donnell.reuben@icloud.com>
2023-03-23 16:39:19 -0500

    learning inputs

---
+++ day2.py
@@ -0,0 +1,15 @@
+input ("What's your name?: ")
+
+
+
+
+
+
+#PYTHON:
+#input ("What's your name?: ")
+
+#RUBY:
+# puts "What's your name?: "
+# input = gets.chomp
+
+#BOTH LINES OF CODE ONLY ASK THE USER TO INPUT THEIR NAME AND DONT DO ANYTHING WITH IT
